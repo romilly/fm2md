747d5c6dd190b53fbb29885acb508ceb7c7277f1
Romilly Cocking <romilly.cocking@gmail.com>
2014-06-22 18:22:46 +0100

    Temporarily add blog stuff I converter using fm2md.

---
+++ data/OTW-Afternoondemos.mm
@@ -0,0 +1,51 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1403325634326" ID="ID_225780265" LINK="OpenTechnologyWorkshop-1.0.1.mm" MODIFIED="1403449247014" TEXT="Afternoon demos">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The afternoon session took place in an upstairs laboratory. It was, in effect, a MiniMaker Faire with concurrent demos spread around the Lab. I really enjoyed the chance to meet the makers and view their projects. I came away with a PyBoard which I am now exploring. More details soon!
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1403419938515" MODIFIED="1403420484640" POSITION="right" TEXT="Richard Bowman">
+<icon BUILTIN="calendar"/>
+<node CREATED="1403419960738" MODIFIED="1403419973472" TEXT="3d Printed Platform for Microscopy"/>
+</node>
+<node CREATED="1403420159730" MODIFIED="1403420522000" POSITION="right" TEXT="Damien George">
+<icon BUILTIN="calendar"/>
+<node CREATED="1403420170546" MODIFIED="1403420196576" TEXT="MicroPython - Python Running on Microcontrollers"/>
+</node>
+<node CREATED="1403420205218" MODIFIED="1403420532456" POSITION="right" TEXT="Josie Hughes and James Ritchie">
+<icon BUILTIN="calendar"/>
+<node CREATED="1403420223858" MODIFIED="1403420238384" TEXT="Automated microscopy and mechanical testing"/>
+</node>
+<node CREATED="1403420245882" MODIFIED="1403420255232" POSITION="right" TEXT="Jakub Nedbal">
+<node CREATED="1403420257194" MODIFIED="1403420260400" TEXT="KCL"/>
+<node CREATED="1403420262858" MODIFIED="1403420281632" TEXT="Open Source 3D printed flow cytometer"/>
+</node>
+<node CREATED="1403420294530" MODIFIED="1403420408130" POSITION="right" TEXT="Pratap Singh">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Pratap is a year 10 student at the Persr School, Cambridge.
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1403420322058" MODIFIED="1403420363471" TEXT="Cosmic-ray muon detection: can we verify a prediction of Special Relativity in the school laborarory?"/>
+</node>
+<node CREATED="1403420413914" MODIFIED="1403420420447" POSITION="right" TEXT="Jonathan Waite">
+<node CREATED="1403420421538" MODIFIED="1403420427599" TEXT="Open Source 3D printing"/>
+</node>
+<node CREATED="1403420431970" MODIFIED="1403420435295" POSITION="right" TEXT="Di Wu">
+<node CREATED="1403420436378" MODIFIED="1403420459216" TEXT="Home monitoring of Lung Function with Raspberry Pi"/>
+</node>
+</node>
+</map>
